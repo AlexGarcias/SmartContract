8307c583bc53d8598b3330fe03918861076f87e9
AlexGarcias <43684829+AlexGarcias@users.noreply.github.com>
2019-08-15 11:52:30 -0300

    Update Token_Standard.sol

--- ERC-20/Token_Standard.sol
+++ ERC-20/Token_Standard.sol
@@ -5,6 +5,23 @@ pragma solidity ^0.5.0;
 // Goal: Create AGGreen Crypto-assets; https://github.com/aggreensmart
 // Create AGPirate Crypto-assets; https://github.com/agpiratesmart
 // Create AGSports Crypto-assets; https://github.com/agsports
+/*
+
+Alex Garcias, Dr. Attorney. Research. Developer C#, Python, Solidity, PHP, HTML, CSS.
+
+Web site: http://alexgarcias.com.br | https://github.com/alexgarcias
+GitHub AGGreen: https://github.com/aggreensmart 
+GitHub AGPirate:  https://github.com/agpiratesmart 
+AGGreen Contact: agg@alexgarcias.com.br 
+Alex Garcias contact: contato@alexgarcias.com.br 
+LinkedIn: www.linkedin.com/in/alexgarcias 
+Twitter: @lexgarcias 
+
+Academia Science 
+
+	https://independent.academia.edu/alexgarcia463 
+
+*/
 
 // https://theethereum.wiki/w/index.php/ERC20_Token_Standard
 
