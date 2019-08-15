26c793e256dbe311c5ab69b8ee882bac99f45e7a
AlexGarcias <43684829+AlexGarcias@users.noreply.github.com>
2019-08-15 18:01:19 -0300

    Update Token_Standard.sol

--- ERC-20/Token_Standard.sol
+++ ERC-20/Token_Standard.sol
@@ -28,7 +28,7 @@ pragma solidity ^0.5.0;
 
 // Safe maths
 
-// ----------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
 library SafeMath {
 
@@ -66,6 +66,16 @@ library SafeMath {
 
 }
 
+
+
+// ----------------------------------------------------------------------------
+
+// ERC Token Standard #20 Interface
+
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+
+// ----------------------------------------------------------------------------
+
 contract ERC20Interface {
 
     function totalSupply() public view returns (uint);
@@ -86,3 +96,78 @@ contract ERC20Interface {
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 
 }
+
+
+
+// ----------------------------------------------------------------------------
+
+// Contract function to receive approval and execute function in one call
+
+//
+
+// Borrowed from MiniMeToken
+
+// ----------------------------------------------------------------------------
+
+contract ApproveAndCallFallBack {
+
+    function receiveApproval(address from, uint256 tokens, address token, bytes memory data) public;
+
+}
+
+
+
+// ----------------------------------------------------------------------------
+
+// Owned contract
+
+// ----------------------------------------------------------------------------
+
+contract Owned {
+
+    address public owner;
+
+    address public newOwner;
+
+
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+
+
+    constructor() public {
+
+        owner = msg.sender;
+
+    }
+
+
+    modifier onlyOwner {
+
+        require(msg.sender == owner);
+
+        _;
+
+    }
+
+
+    function transferOwnership(address _newOwner) public onlyOwner {
+
+        newOwner = _newOwner;
+
+    }
+
+    function acceptOwnership() public {
+
+        require(msg.sender == newOwner);
+
+        emit OwnershipTransferred(owner, newOwner);
+
+        owner = newOwner;
+
+        newOwner = address(0);
+
+    }
+
+}
+
+
+
