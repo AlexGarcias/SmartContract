e1ba83d4ceb4c4b396704ff7c7efdd0733551b78
AlexGarcias <43684829+AlexGarcias@users.noreply.github.com>
2019-08-18 19:33:45 -0300

    Create Member.sol

---
+++ AddPerson/Member.sol
@@ -0,0 +1,29 @@
+pragma solidity >=0.5.1 <0.7.0;
+
+// Register member in blockchain Ethereum
+// The member has your own Hash
+// The register Member Hash is linked with the other Smart Contract
+// to controle AGGreen / AGPirate / AGSports
+// This contract is draft
+
+contract MyMembers {
+    
+    Person[] public people;
+    uint256 public peopleAccount;
+    
+  
+    // Defines a new type with two fields.
+    
+    struct Person {
+        string _firstname;
+        string _lastname;
+    }
+    
+    function addPerson(string  memory _firstname , string memory _lastname) public {
+        
+        people.push(Person(_lastname , _firstname));
+         peopleAccount +=1;
+        
+    }
+
+}
