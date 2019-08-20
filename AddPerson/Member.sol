1868c19e90c1b45eb83fb28888697d08d38cff9e
AlexGarcias <43684829+AlexGarcias@users.noreply.github.com>
2019-08-20 16:39:46 -0300

    Update Member.sol

--- AddPerson/Member.sol
+++ AddPerson/Member.sol
@@ -7,24 +7,64 @@ pragma solidity >=0.5.1 <0.7.0;
 // This contract is draft
 // Start by Alex Garcias, Dr. https://alexgarcias.com.br 
 
+pragma solidity >=0.5.1 <0.7.0;
+
 contract MyMembers {
+
+     address payable member;
+    
+    // public data
     
     Person[] public people;
     uint256 public peopleAccount;
-    
-  
-    // Defines a new type with two fields.
-    
+
+// private data
+
+
+    PersonPrivate[] public PPrivate;
+   
+   // Defines public data about members
+
     struct Person {
-        string _firstname;
-        string _lastname;
+	
+        
+    	string firstname;
+        string lastname;
+        string mothername;
+        string fathername;
+        string photo;
+        string country;
+
     }
     
-    function addPerson(string  memory _firstname , string memory _lastname) public {
+    function addPerson(string  memory _firstname , string memory _lastname , string memory _mothername ,string memory _fathername , string memory _photo , string memory _country) public {
         
-        people.push(Person(_lastname , _firstname));
+        people.push(Person(_firstname , _lastname , _mothername , _fathername , _photo , _country));
          peopleAccount +=1;
         
     }
 
+// private data
+
+struct PersonPrivate {
+
+string Doc;
+
 }
+
+
+    function pperson(string memory _Doc) public {
+        PPrivate.push(PersonPrivate (_Doc));
+        
+        
+    }
+
+
+
+// Modifier 
+
+
+  
+
+}// final contrac 
+
