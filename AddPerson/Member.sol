879740ad595d692fe4ab0ab614d37731675f94f1
AlexGarcias <43684829+AlexGarcias@users.noreply.github.com>
2019-08-25 14:32:01 -0300

    Update Member.sol

--- AddPerson/Member.sol
+++ AddPerson/Member.sol
@@ -17,6 +17,19 @@ contract Owner {
 
 	uint256 public peopleCount = 0;// Count person registered in Blockchain
 	mapping (uint => Person) public people; // Mapping Struct Person to stock data 
+	address payable owner;
+	
+// Modifer ***********************************
+
+//modifier onlyOwner () {
+
+//	require (msg.sender == owner);
+
+//	_;
+//}
+
+
+// Modifer ***********************************
 
 struct Person {
 
@@ -25,21 +38,28 @@ struct Person {
 	string lastName;
 	string wallet;
 	string photo;
-	string email
+	string email;
 }
+
+//constructor() public {
+
+//owner = msg.sender;
+
+//}
 	
 function addPerson(string memory _firstName, string memory _lastName, string memory _wallet, string memory _photo, string memory _email) public {
 
-peopleCount +=1;
+increementCount();
 people[peopleCount] = Person(peopleCount, _firstName , _lastName , _wallet, _photo , _email);
 
 
 }
 
+function increementCount () internal {
 
+peopleCount +=1;
 
-
-   }// Final Contract
+}
 
 
 
