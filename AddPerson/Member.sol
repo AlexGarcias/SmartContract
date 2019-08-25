8b06400d63f46bc78536d1d84ca557518add0905
AlexGarcias <43684829+AlexGarcias@users.noreply.github.com>
2019-08-25 07:41:09 -0300

    Update Member.sol

--- AddPerson/Member.sol
+++ AddPerson/Member.sol
@@ -7,22 +7,29 @@ pragma solidity >=0.5.1 <0.7.0;
 // This contract is draft
 // Start by Alex Garcias, Dr. https://alexgarcias.com.br 
 
+// ********************* Reference ***********************************
+// https://solidity.readthedocs.io/en/v0.5.11/types.html | Solidity 0.5.11 Docs
+
+
+//********************** Final Reference *****************************
+
 contract Owner {
 
-	uint256 public peopleCount = 0;
+	uint256 public peopleCount = 0;// Count person registered in Blockchain
+	uint256 public contactCount = 0;// Count contact person;
 	mapping (uint => Person) public people; // Mapping Struct Person to stock data 
 
 struct Person {
 
-	uint id;
+	uint   id;
 	string firstName;
 	string lastName;
 	string wallet;
 	string photo;
-	string country;
+	string email
 }
 	
-function addPerson(string memory _firstName, string memory _lastName, string memory _wallet, string memory _photo, string memory _country) public {
+function addPerson(string memory _firstName, string memory _lastName, string memory _wallet, string memory _photo, string memory _email) public {
 
 peopleCount +=1;
 people[peopleCount] = Person(peopleCount, _firstName , _lastName , _wallet, _photo , _country);
@@ -31,8 +38,9 @@ people[peopleCount] = Person(peopleCount, _firstName , _lastName , _wallet, _pho
 }
 
 
-   }// Final Contract	
 
+
+   }// Final Contract
 
 
 
